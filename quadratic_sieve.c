16b72acfe64b0fb85c5eb110aa895b72881baf4f
AppSupportAPI <schulz.kilian@outlook.de>
2024-02-17 11:19:43 +0100

    more small stuff

--- quadratic_sieve.c
+++ quadratic_sieve.c
@@ -32,8 +32,7 @@ void factor_primes(long long n) {
     free(relations);*/
 }
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
+void gpuAssert(cudaError_t code, const char *file, int line, bool abort) {
    if (code != cudaSuccess) {
       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
       if (abort) exit(code);
