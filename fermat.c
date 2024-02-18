897a1a9c9c1709efa91c147f6fbf61be25cdc9c3
AppSupportAPI <schulz.kilian@outlook.de>
2024-02-18 16:23:08 +0100

    trying better parallelization

--- fermat.c
+++ fermat.c
@@ -1,24 +1,26 @@
 #include <math.h>
 #include <omp.h>
+#include <stdio.h>
 
 void compute_primes(long long N, long long* result, long long* factor) {
-    long long a = ceil(sqrt(N));
-    long long b2 = a * a - N;
-    long long b = round(sqrt(b2));
+    long long a_start = ceil(sqrt(N));
+    long long a_end = a_start + 10000; // A predetermined range for demonstration
     int found = 0;
 
-    #pragma omp parallel for private(b2, b) shared(a, found)
-    for (long long a = ceil(sqrt(N)); !found; a++) {
-        b2 = a * a - N;
-        b = round(sqrt(b2));
+    #pragma omp parallel for
+    for (long long a = a_start; a <= a_end; ++a) {
+        if (found) continue; // Skip iteration if factors are already found
+
+        long long b2 = a * a - N;
+        long long b = round(sqrt(b2));
 
         if (b * b == b2) {
             #pragma omp critical
             {
                 if (!found) { // Ensure only the first solution is taken
-                    *factor = a + b;
                     *result = a - b;
-                    found = 1; // Signal to other threads that we found the factors
+                    *factor = a + b;
+                    found = 1;
                 }
             }
         }
